d103001cf432043d26b957edebb061f1ab4ec60f
Angger Bayu Pangestu <kontak@angger.me>
2021-03-20 14:27:14 +0700

    :pencil: add materi : slice

--- Working-with-string/script.js
+++ Working-with-string/script.js
@@ -10,3 +10,13 @@ console.log(kata.indexOf("KalimAt"));
 // ----- Method lastIndexOf() -----
 /* LastindexOf -> Menampilkan indek array yang terakhir */
 console.log(kata.lastIndexOf("i"));
+
+// ----- Method slice() -----
+/* slice -> memotong string dari urutan ke  */
+console.log(kata.slice(4));
+/* slice -> memotong string dari urutan ke sampai sebelum urutan ke  */
+console.log(kata.slice(4, 10));
+/* memotong spasi (' ')  */
+console.log(kata.slice(0, kata.indexOf(" ")));
+/* memotong spasi dari spasi paling terakhir +1 untuk menambah karakter setelah spasi ("")  */
+console.log(kata.slice(kata.lastIndexOf(" ") + 1));
