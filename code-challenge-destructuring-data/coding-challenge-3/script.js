7cda3f2659a473857cc9ae766561f310766201c2
Angger Bayu Pangestu <kontak@angger.me>
2021-03-20 11:06:06 +0700

    add repo : coding-challenge-3

---
+++ code-challenge-destructuring-data/coding-challenge-3/script.js
@@ -0,0 +1,42 @@
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimich",
+      "Goretzka",
+      "Gosan",
+      "Muller",
+      "Garrby",
+      "Lewandowski",
+    ],
+
+    [
+      "Marwin",
+      "Nico",
+      "Axel",
+      "Mahmoud",
+      "Manuel",
+      "Marcel",
+      "Emre can",
+      "Carvalho",
+      "Hazard",
+      "Brandt",
+      "Reus",
+    ],
+  ],
+
+  score: "4:0",
+  scored: ["Lewandowski", "Garrby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2031",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
