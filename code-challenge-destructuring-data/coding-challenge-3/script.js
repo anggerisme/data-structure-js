3010a33e2145d6c72e4bce216d2327454019cede
Angger Bayu Pangestu <kontak@angger.me>
2021-03-20 12:17:25 +0700

    add challenge : count the events itself

--- code-challenge-destructuring-data/coding-challenge-3/script.js
+++ code-challenge-destructuring-data/coding-challenge-3/script.js
@@ -40,3 +40,21 @@ const game = {
     team2: 6.5,
   },
 };
+
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🟨 Yellow card"],
+  [69, "🟥Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🟨 Yellow card"],
+]);
+
+// Count the events itself
+const newSet = new Set(gameEvents.values());
+console.log(newSet);
