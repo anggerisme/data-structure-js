29e893682d7eb2b79ab98cefb8b9accfbd120a03
Angger Bayu Pangestu <kontak@angger.me>
2021-03-20 14:23:01 +0700

    :file_folder: add repo : working with string

---
+++ Working-with-string/script.js
@@ -0,0 +1,8 @@
+const kata = "ini adalah kalimAt";
+console.log(kata.length);
+
+// ----- Method indexOf() -----
+/* IndexOf -> Menampilkan indek array yang pertama */
+console.log(kata.indexOf("a")); //5
+/* Jika tidak ditemukan akan menampilkan -1 */
+console.log(kata.indexOf("KalimAt"));
