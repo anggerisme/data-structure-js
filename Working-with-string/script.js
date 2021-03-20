4d71de9e86c4cb780b37b8295c5a26eb08c6802a
Angger Bayu Pangestu <kontak@angger.me>
2021-03-20 14:26:32 +0700

    :pencil: add materi : lastIndexOf

--- Working-with-string/script.js
+++ Working-with-string/script.js
@@ -1,8 +1,12 @@
 const kata = "ini adalah kalimAt";
 console.log(kata.length);
 
-// ------ Method indexOf() ------
+// ----- Method indexOf() -----
 /* IndexOf -> Menampilkan indek array yang pertama */
 console.log(kata.indexOf("a")); //5
 /* Jika tidak ditemukan akan menampilkan -1 */
 console.log(kata.indexOf("KalimAt"));
+
+// ----- Method lastIndexOf() -----
+/* LastindexOf -> Menampilkan indek array yang terakhir */
+console.log(kata.lastIndexOf("i"));
