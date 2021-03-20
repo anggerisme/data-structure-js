1d56ff5a8c6687d9602792853994616bd92ff163
Angger Bayu Pangestu <kontak@angger.me>
2021-03-20 14:25:28 +0700

    :pencil: add materi : indexOf

--- Working-with-string/script.js
+++ Working-with-string/script.js
@@ -1,7 +1,7 @@
 const kata = "ini adalah kalimAt";
 console.log(kata.length);
 
-// ----- Method indexOf() -----
+// ------ Method indexOf() ------
 /* IndexOf -> Menampilkan indek array yang pertama */
 console.log(kata.indexOf("a")); //5
 /* Jika tidak ditemukan akan menampilkan -1 */
